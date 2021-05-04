87dc1d980241ef87ba4c27615c82801e67668285
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-05-04 15:37:20 +0530

    Update Code.js

--- js/Code.js
+++ js/Code.js
@@ -10,16 +10,11 @@ function clues() {
     fill("lightblue")
     text("Hint: Always changing, not constant !!", 100,70)
 
-    fill("white")
-    textSize(15)
-    text("C U T N I F O N", 700,150)
-    fill("lightblue")
-    text("Hint: Performs a particular task !!", 700,170)
-
-    fill("white")
-    textSize(15)
-    text("A T E D A S B A", 100,250)
-    fill("lightblue")
-    text("Hint: Stores all information !!", 100,270)
+   
+    
+    
+    
+    
+    
 
 }
