baa68358084d9480bd27cccc5982d4645782a07b
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-05-04 15:36:40 +0530

    Update Security.js

--- js/Security.js
+++ js/Security.js
@@ -10,21 +10,10 @@ class Security {
         this.button1.position(100,120);
         this.button1.style('background', 'lightgrey');    
 
-        this.access2 = createInput("Code1")
-        this.access2.position(700,190);
-        this.access2.style('background', 'white');  
-
-        this.button2 = createButton('Check');
-        this.button2.position(700,220);
-        this.button2.style('background', 'lightgrey');
-
-        this.access3 = createInput("Code1")
-        this.access3.position(100,290);
-        this.access3.style('background', 'white');  
-
-        this.button3 = createButton('Check');
-        this.button3.position(100,320);
-        this.button3.style('background', 'lightgrey'); 
+       
+        
+        
+        
     }
 
     display(){
@@ -37,21 +26,11 @@ class Security {
             }
         });
 
-        this.button2.mousePressed(() => {
-            if(system.authenticate(accessCode2,this.access2.value())){
-                this.button2.hide();
-                this.access2.hide();
-                score++;
-            }
-        });
-
-        this.button3.mousePressed(() => {
-            if(system.authenticate(accessCode3,this.access3.value())){
-                this.button3.hide();
-                this.access3.hide();
-                score++;
-            }
-        });
+       
+        
+        
+        
+        
 
     }
 }
